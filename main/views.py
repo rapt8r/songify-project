7a1c22449cab95a419fc69c52c07f0a8623019f7
sokalskimichal <101504858+kulturmatyk@users.noreply.github.com>
2022-09-29 19:12:06 +0200

    added MainPage to Views, main_page to urls, main_page.html. Deleted two lines (icons) from root_template.html

--- main/views.py
+++ main/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from main.models import Song
+from random import choice, choices
+from main.models import Author
 
 # Create your views here.
+
+MOTD_TEXT = [
+    'Songify - to co lubisz, tak jak chcesz',
+    'Songify - zabierz ze sobą',
+    'Songify - twoja lista',
+    'Songify - tak jak lubisz',
+    'Songify - muzyka dla ciebie',
+    'Songify - twoja muzyka',
+    'Songify - twoje utwory',
+    'Songify - słuchaj muzyki',
+]
+def MainPage(request):
+
+    context_dir = {
+        'trending_song': Song.objects.all().order_by('-number_of_plays')[0],
+        'top_songs': Song.objects.all().order_by('-number_of_plays')[0:5],
+        'recommended_artists': choices(Author.objects.all(), k=2),
+        'motd': choice(MOTD_TEXT)
+    }
+    return render(request, template_name='main/main_page.html', context=context_dir)
