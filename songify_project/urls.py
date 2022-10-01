81e69f6ead5dc1d25e5ecf04b99f44a739ace491
Dominik Ciupa <20306035+rapt8r@users.noreply.github.com>
2022-10-01 11:23:28 +0200

    Fixed static files

--- songify_project/urls.py
+++ songify_project/urls.py
@@ -18,7 +18,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from main.views import MainPage
-from main.models import Song
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +25,4 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
