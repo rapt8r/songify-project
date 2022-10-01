9fe276d8646ba34d4472a65a899901637ab7fd7e
Dominik Ciupa <20306035+rapt8r@users.noreply.github.com>
2022-10-01 18:23:15 +0200

    Searching feature added (../search/?q=Theme Song)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from main.models import Song, Author, Playlist
 # Create your views here.
+
+class SearchPage(TemplateView):
+    template_name = 'main/search_page.html'
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        #Pobieranie parametru q z adresu url (np: songify.com/search?q=)
+        query = request.GET.get('q', 'all')
+        context['songs_search_result'] = Song.objects.filter(title__contains=query).all()
+        context['authors_search_result'] = Author.objects.filter(name__contains=query).all()
+        context['playlists_search_result'] = Playlist.objects.filter(name__contains=query).all()
+        return self.render_to_response(context)
--- songify_project/urls.py
+++ songify_project/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import SearchPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', SearchPage.as_view(), name='SearchPage')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
