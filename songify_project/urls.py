7260691bcd5a2629b20ccf91868c5b7241cb7b4a
sokalskimichal <101504858+kulturmatyk@users.noreply.github.com>
2022-10-02 13:35:27 +0200

    init all songs page and template

--- songify_project/urls.py
+++ songify_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from main.views import AllSongsPage
 from main.views import SearchPage
 from main.views import MainPage
 
